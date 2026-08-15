8102cef3c3b0ede1b31d5a20115c6eeb3dd2ed97
Tymcftw <45132767+tymcftw@users.noreply.github.com>
2026-08-15 01:25:54 -0500

    2 tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,16 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # source (/usr/bin/starship init fish --print-full-init | psub)
 
 # start ssh-agent if not running
+
 if test -z "$SSH_AUTH_SOCK"
     eval (ssh-agent -s)
     set -x SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
 
-if test -n "$SSH_AUTH_SOCK"
-    ssh-add -l >/dev/null 2>&1; or ssh-add ~/.ssh/id_rsa >/dev/null 2>&1
-end
-
 # Export SSH_AUTH_SOCK from systemd user runtime dir
 # set -x SSH_AUTH_SOCK (printf '%s/ssh-agent.sock' (systemd --user show-environment 2>/dev/null; echo $XDG_RUNTIME_DIR) )
 # Fallback if above fails
+
 if test -z "$SSH_AUTH_SOCK"
     set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.sock
 end
@@ -37,8 +35,11 @@ end
 # pnpm end
 
 alias dotfiles 'git --git-dir=$DOTFILES_PATH/ --work-tree=$HOME'
+
 set DOTFILES_PATH "$HOME/.dotfiles/"
 set PATH /home/tymcftw/.cargo/bin /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin /usr/local/bin /usr/bin /bin /home/tymcftw/.local/bin /home/tymcftw/.local/share/pnpm /home/tymcftw/.local/share/swiftly/bin /usr/local/sbin /var/lib/flatpak/exports/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+set SOPS_EDITOR nano
+set EDITOR nano
 
 function hex2ascii
     echo $argv | string replace -a ' ' '' | xxd -r -p
