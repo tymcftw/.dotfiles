3b35dc427dec7d41e2cae6caed8d342f930d2495
Tymcftw <45132767+tymcftw@users.noreply.github.com>
2026-08-21 18:46:40 -0500

    x

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # source (/usr/bin/starship init fish --print-full-init | psub)
 
 ## -- Tide Prompt Configuration ---------------------------------------- ##
-
 # tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='12-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two
 # lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
 
@@ -11,9 +10,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 ####################################
 ## Start SSH Agent if not running ##
 ####################################
-
 # Export SSH_AUTH_SOCK from systemd user runtime dir
-set -x SSH_AUTH_SOCK (printf '%s/ssh-agent.sock' (systemctl --user show-environment 2>/dev/null; echo $XDG_RUNTIME_DIR))
+# set -x SSH_AUTH_SOCK (printf '%s/ssh-agent.sock' (systemctl --user show-environment 2>/dev/null; echo $XDG_RUNTIME_DIR))
 
 #############################
 ## Fallback if above fails ##
@@ -34,7 +32,6 @@ set -x SSH_AUTH_SOCK (printf '%s/ssh-agent.sock' (systemctl --user show-environm
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
 
 ## -- Begin pnpm ------------------------------------------------------- ## 
-
 set -gx PNPM_HOME "/home/tymcftw/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
@@ -43,7 +40,6 @@ end
 ## -- End pnpm --------------------------------------------------------- ##
 
 ## -- Aliases ---------------------------------------------------------- ##
-
 set DOTFILES_PATH "$HOME/.dotfiles/"
 alias dotfiles 'git --git-dir=$DOTFILES_PATH/ --work-tree=$HOME'
 set PATH /home/tymcftw/.cargo/bin /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin /usr/local/bin /usr/bin /bin /home/tymcftw/.local/bin /home/tymcftw/.local/share/pnpm /home/tymcftw/.local/share/swiftly/bin /usr/local/sbin /var/lib/flatpak/exports/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
@@ -55,7 +51,6 @@ set PAGER more
 ## -- End Aliases ------------------------------------------------------ ##
 
 ## -- Functions -------------------------------------------------------- ##
-
 ###############
 ## Hex2ascii ##
 ###############
